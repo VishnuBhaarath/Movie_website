fb10402dacb29ec923db90c4c28e605d8a3f424b
VishnuBhaarath <vishnubhaarath30@gmail.com>
2022-02-04 17:34:05 +0530

    Adding toggle

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const arrows=document.querySelectorAll(".arrow");
 const movieLists=document.querySelectorAll(".movie-list-items");
 
 for(let i=0;i<arrows.length;i++){
+	const no_of_items=movieLists[i].querySelectorAll("img").length;
+	let counter=0;
+	console.log(no_of_items);
 	arrows[i].addEventListener("click",()=>{
+		const ratio=Math.floor(window.innerWidth/270);
+		counter++;
+		if(no_of_items-(4+counter)+(4-ratio)>=0){
 		movieLists[i].style.transform = `translateX(${
         movieLists[i].computedStyleMap().get("transform")[0].x.value-300
-      }px)`;
+      }px)`;}
+		else{
+			movieLists[i].style.transform="translateX(0)";
+			counter=0;
+		}
 		
 	})
 }
 	
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container",".movie-list-title",".movie-list-container"
+);	
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  
+});
+
 
 
